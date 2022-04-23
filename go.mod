0a7aac2f32105221e7707d079cc56f7f732e612f
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-04-23 14:56:38 +0700

    Added firstName and lastName on SayHello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/widekaardhiguna/go-say-hello
+module github.com/widekaardhiguna/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello" + name
+func SayHello(firstName string, lastName string) string {
+	return "Hello " + firstName + " " + lastName
 }
